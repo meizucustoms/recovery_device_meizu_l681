a8754dc33c5e1cc72e636fb27429c075fb29f6de
leskal <alexakis1997@gmail.com>
2017-03-11 20:09:19 +0200

    Remove unused line

--- product/prebuilt_kernel.mk
+++ product/prebuilt_kernel.mk
@@ -1,4 +1,3 @@
 # Prebuilt kernel
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/kernel:kernel \
-    $(LOCAL_PATH)/prebuilts/fuelgauged_static:sbin/fuelgauged_static
+    $(LOCAL_PATH)/prebuilts/kernel:kernel

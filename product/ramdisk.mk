eb246a3cdb8d82d47e5dd10a604e40c214fd175f
leskal <alexakis1997@gmail.com>
2017-07-26 17:44:06 +0300

    Simplify the tree

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,7 +1,7 @@
 # Ramdisk
 PRODUCT_PACKAGES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.mt6755:root/fstab.mt6755 \
-    $(LOCAL_PATH)/rootdir/etc/init.recovery.mt6755.rc:root/init.recovery.mt6755.rc
+    $(LOCAL_PATH)/rootdir/fstab.mt6755:root/fstab.mt6755 \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6755.rc:root/init.recovery.mt6755.rc
 
 # F2FS
 PRODUCT_PACKAGES += \
